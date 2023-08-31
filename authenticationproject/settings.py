1f888579e5a9397f0485d7cb8b9e0dccb3cb9fde
hardeeelixir <hardee.raval@elixirtechne.com>
2023-08-31 17:05:31 +0530

    Add static files.

--- authenticationproject/settings.py
+++ authenticationproject/settings.py
@@ -11,7 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qwjd+btsb5%ra_p+hky#3bagij26i3!%p%q+y@z#$8(efpi-^_'
+SECRET_KEY = env('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-qwjd+btsb5%ra_p+hky#3bagij26i3!%p%q+y@z#$8(efpi-^_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
