9c5a2d0e292e20c97212f02b53a11169dcaaff3f
hardeeelixir <hardee.raval@elixirtechne.com>
2023-08-31 15:59:10 +0530

    Add static files.

--- apps/authapp/urls.py
+++ apps/authapp/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('signup', views.signup, name='signup'),
-    path('', views.sign, name='sign'),
+    path('', views.home, name='home'),
     path('cards', views.cards, name='cards'),
     path('base', views.base, name='base'),
     path('support', views.support, name='support'),
     path('support_file', views.support_file, name='support_file')
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
